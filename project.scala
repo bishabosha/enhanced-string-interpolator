9f2c224422a90477563b81ecf6055a389ca567c7
Jamie Thompson <bishbashboshjt@gmail.com>
2024-02-02 00:28:27 +0100

    add publish details

--- project.scala
+++ project.scala
@@ -1,4 +1,19 @@
 //> using scala "3.4.0-RC3"
 //> using test.dep org.scalameta::munit:1.0.0-M10
+//> using publish.organization "io.github.bishabosha"
+//> using publish.name "enhanced-string-interpolator"
+//> using publish.computeVersion "git:tag"
+//> using publish.repository "central-s01"
+//> using publish.license "Apache-2.0"
+//> using publish.url "https://github.com/bishabosha/enhanced-string-interpolator"
+//> using publish.versionControl "github:bishabosha/enhanced-string-interpolator"
+//> using publish.developer "bishabosha|Jamie Thompson|https://github.com/bishabosha"
+//> using publish.ci.computeVersion "git:tag"
+//> using publish.ci.repository "central-s01"
+//> using publish.ci.user "env:PUBLISH_USER"
+//> using publish.ci.password "env:PUBLISH_PASSWORD"
+//> using publish.ci.secretKey "env:PUBLISH_SECRET_KEY"
+//> using publish.ci.secretKeyPassword "env:PGP_PASSWORD"
+//> using publish.ci.publicKey "env:PUBLISH_PUBLIC_KEY"
 
 // currently the compiler crashes in 3.3.1, so we use 3.4.0-RC3
